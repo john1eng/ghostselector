954943638a9a5b74ada938703347a3443fb96862
john1eng <john1eng@hotmail.com>
2019-10-31 19:52:07 -0400

    second commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, {Component} from 'react';
+
+
+
+class Selector extends Component{
+  render(){
+    return (
+      <div className="container">
+        <div className="navbar">
+          <div>Selector: </div>
+          <div>What goes here?</div>
+        </div>
+        <div className="ghost-list">Put the Ghost component here</div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default Selector;
---
+++ src/Ghost.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Ghost = (props) => {
+  return(
+    <div>
+    </div>
+  )
+}
+
+export default Ghost
