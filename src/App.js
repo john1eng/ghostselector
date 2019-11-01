ad118437566da9fe3a2f864617c6ef81db766e3b
john1eng <john1eng@hotmail.com>
2019-10-31 20:42:21 -0400

    created a method for the click to represent the right ghost & added pictures and finished the Ghost Component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,40 @@
 
 import React, {Component} from 'react';
-
+import Ghost from './Ghost';
+import ghost1 from './images/ghost1.png';
+import casper from './images/casper.jpeg';
+import marshmallowghost from './images/marshmallowghost.jpeg';
 
 
 class Selector extends Component{
+  constructor(){
+    super();
+    this.state = {
+      selectedGhost: 'casper',
+    }
+  }
+
+selectGhost = (ghostName) => {
+  this.setState({
+    selectedGhost: ghostName,
+  });
+};
+
+
+
+
   render(){
     return (
       <div className="container">
         <div className="navbar">
           <div>Selector: </div>
-          <div>What goes here?</div>
+          <div>{this.state.selectedGhost}</div>
         </div>
-        <div className="ghost-list">Put the Ghost component here</div>
+        <div className="ghost-list">
+        <Ghost ghost={ghost1} selectGhost={this.selectGhost}/>
+        <Ghost ghost={casper} selectGhost={this.selectGhost}/>
+        <Ghost ghost={marshmallowghost} selectGhost={this.selectGhost}/>
+        </div>
       </div>
     )
   }
--- src/Ghost.js
+++ src/Ghost.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
 const Ghost = (props) => {
+  const selectGhost = props.selectGhost;
+  const ghost = props.ghost;
+
   return(
     <div>
+      <img src={ghost} alt="" onClick={() => selectGhost(ghost)}/>
     </div>
-  )
-}
+  );
+};
 
 export default Ghost
